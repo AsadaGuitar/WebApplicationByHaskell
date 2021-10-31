2960e432712586296b658cebd6924df41a3acf59
AsadaGuitar <79627592+AsadaGuitar@users.noreply.github.com>
2021-10-31 19:43:37 +0900

    defined path handler

--- CheckProject/app/Main.hs
+++ CheckProject/app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import qualified Codec.Binary.UTF8.String as UTF8
 
+import qualified Data.Text as T
+
 import qualified Data.ByteString as DB
 import qualified Data.Binary.Builder as DBB
 
@@ -11,13 +13,47 @@ import qualified Network.HTTP.Types as HTypes
 
 
 main :: IO ()
-main =  Warp.run 8000 helloApp
+main =  Warp.run 8000 startServer
 
+startServer :: Wai.Application
+startServer req = router (Wai.pathInfo req) req
+
+
+fooText :: [T.Text]
+fooText = [T.pack "foo"]
+
+booText :: [T.Text]
+booText = [T.pack "boo"]
+
+router :: [T.Text] -> Wai.Application
+router txts req send
+    | txts == fooText  = fooApp req send
+    | txts == booText = booApp req send
+    | otherwise             = helloApp req send
+
+echoApp :: [T.Text] -> Wai.Application
+echoApp txts req send 
+    = send $ Wai.responseBuilder HTypes.status200 [] $ strGetBuilder $ T.unpack (head txts)
+
+notFoundApp :: Wai.Application
+notFoundApp req send
+    = send $ Wai.responseBuilder HTypes.status200 [] $ strGetBuilder "not found."
+    
+
+booApp :: Wai.Application
+booApp req send 
+    = send $ Wai.responseBuilder HTypes.status200 [] $ strGetBuilder "boooo !"
+   
+fooApp :: Wai.Application
+fooApp req send 
+    = send $ Wai.responseBuilder HTypes.status200 [] $ strGetBuilder "foooo !"
+   
 helloApp :: Wai.Application
-helloApp req send = 
-    let build = DBB.fromByteString $ toByteString "Hello Wai !!"
-    in 
-        send $ Wai.responseBuilder HTypes.status200 [] build    
+helloApp req send 
+    = send $ Wai.responseBuilder HTypes.status200 [] $ strGetBuilder "Hello World !"
+
+strGetBuilder :: String -> DBB.Builder
+strGetBuilder str = DBB.fromByteString $ toByteString str
 
 toByteString :: String -> DB.ByteString
 toByteString s = DB.pack $ UTF8.encode s
